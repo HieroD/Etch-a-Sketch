3595e2fdfbe87b74ed273decc6af3621c0b950df
Hiero <hiero.dhp@gmail.com>
2026-04-02 19:55:21 +0700

    Added getRandomRgb() function to randomize the color of initial paint and darkenColor() function to darken the color of trailing cells

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Global variable
 const MIN_SIZE = 16;
 const MAX_SiZE = 100;
+const MAX_RGB = 255;
+const MIN_RGB = 0;
 const gridContainer = document.getElementById("container-grid");
-let gridSize;
 let currentMode = "paint";
-
+let gridSize;
 
 function createGridElements() {
     const inputNumber = document.getElementsByName("grid")[0];
@@ -32,14 +33,40 @@ function createGridElements() {
     
 }
 
+function getRandomRgb() {
+    const r = Math.floor(Math.random() * (MAX_RGB + 1));
+    const g = Math.floor(Math.random() * (MAX_RGB + 1));
+    const b = Math.floor(Math.random() * (MAX_RGB + 1));
+    const rgbValue = `rgb(${r}, ${g}, ${b})`
+    return rgbValue;
+}
+
+function darkenColor(rgbColor) {
+    let [r, g, b] = rgbColor.match(/\d+/g).map(Number);
+    const DARKEN = 20;
+    r -= DARKEN;
+    g -= DARKEN;
+    b -= DARKEN;
+
+    if(r < MIN_RGB) r = MIN_RGB;
+    if(g < MIN_RGB) g = MIN_RGB;
+    if(b < MIN_RGB) b = MIN_RGB;
+    
+    const rgbString = `rgb(${r}, ${g}, ${b})`
+    return rgbString;
+}
+
 // modying grid element's background color
 gridContainer.addEventListener("mouseover", (e) => {
     if(e.target.id === "grid-element"){
+        let currentColor = window.getComputedStyle(e.target).backgroundColor;
+
         if(currentMode === "paint"){
-            e.target.style["background-color"] = "black";
-        } 
+            if(currentColor === "rgba(0, 0, 0, 0)" || currentColor === "rgb(255, 255, 255)" || currentColor === "transparent") e.target.style.backgroundColor  = getRandomRgb();
+            else {e.target.style.backgroundColor = darkenColor(currentColor);}
+        }
         else if(currentMode === "erase") {
-            e.target.style["background-color"] = "white";
+            e.target.style.backgroundColor  = "white";
         }
         else if (currentMode === "view"){}
     }
